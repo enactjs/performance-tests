015cbeff68551a038c011612002a38bc9ed9f976
Daniel Stoian <daniel.stoian@lgepartner.com>
2021-09-07 12:19:32 +0300

    added new tests

--- performance/tests/Scroller.test.js
+++ performance/tests/Scroller.test.js
@@ -1,4 +1,5 @@
-const {FPS, Mount} = require('../TraceModel');
+const getCustomMetrics = require('../ProfilerMetrics');
+const {DCL, FCP, FPS} = require('../TraceModel');
 const {getFileName, scrollAtPoint} = require('../utils');
 const TestResults = require('../TestResults');
 
@@ -45,18 +46,77 @@ describe( 'Scroller', () => {
 		});
 	});
 
-	it('mount', async () => {
-		const filename = getFileName('Scroller');
+	it('mount time', async () => {
+		const filename = getFileName(component);
 
+		await page.goto('http://localhost:8080/switchItem');
 		await page.tracing.start({path: filename, screenshots: false});
-		await page.goto('http://localhost:8080/scroller');
-		await page.waitFor(2000);
+		await page.waitForSelector('#switchItem');
 
 		await page.tracing.stop();
 
-		const actual = Mount(filename, 'Scroller');
+		const actualMountTime = (await getCustomMetrics(page))['mount'];
+		TestResults.addResult({component: component, type: 'Mount Time', actualValue: actualMountTime});
+	});
 
-		TestResults.addResult({component: 'Scroller', type: 'Mount', actualValue: actual});
+
+	it('should have a good FCP', async () => {
+		const filename = getFileName(component);
+
+		let cont = 0;
+		let avg = 0;
+		for (let step = 0; step < stepNumber; step++) {
+			const FCPPage = await testMultiple.newPage();
+
+			await FCPPage.tracing.start({path: filename, screenshots: false});
+			await FCPPage.goto('http://localhost:8080/switchItem');
+			await FCPPage.waitForSelector('#switchItem');
+
+			await FCPPage.tracing.stop();
+
+			const actualFCP = await FCP(filename);
+			avg = avg + actualFCP;
+
+			if (actualFCP < maxFCP) {
+				cont += 1;
+			}
+			await FCPPage.close();
+		}
+		avg = avg / stepNumber;
+
+		TestResults.addResult({component: component, type: 'average FCP', actualValue: avg});
+
+		expect(cont).toBeGreaterThan(percent);
+		expect(avg).toBeLessThan(maxFCP);
+	});
+
+	it('should have a good DCL', async () => {
+		const filename = getFileName(component);
+
+		let cont = 0;
+		let avg = 0;
+		for (let step = 0; step < stepNumber; step++) {
+			const DCLPage = await testMultiple.newPage();
+			await DCLPage.tracing.start({path: filename, screenshots: false});
+			await DCLPage.goto('http://localhost:8080/switchItem');
+			await DCLPage.waitForSelector('#switchItem');
+
+			await DCLPage.tracing.stop();
+
+			const actualDCL = await DCL(filename);
+			avg = avg + actualDCL;
+
+			if (actualDCL < maxDCL) {
+				cont += 1;
+			}
+			await DCLPage.close();
+		}
+		avg = avg / stepNumber;
+
+		TestResults.addResult({component: component, type: 'average DCL', actualValue: avg});
+
+		expect(cont).toBeGreaterThan(percent);
+		expect(avg).toBeLessThan(maxDCL);
 	});
 
 	describe('mount with various children', () => {
@@ -65,9 +125,7 @@ describe( 'Scroller', () => {
 		let results = [];
 		const types = [
 			'ScrollerJS',
-			'ScrollerNative',
-			'UiScrollerJS',
-			'UiScrollerNative'
+			'UiScrollerJS'
 		];
 
 		for (const type of types) {
@@ -85,6 +143,9 @@ describe( 'Scroller', () => {
 					const actual = Mount(filename, 'ScrollerMultipleChildren');
 					results.push({count: count, value: actual, type: type});
 					TestResults.addResult({component: 'Scroller', type: `Mount ${count} ${type}`, actualValue: actual});
+
+					const actualMountTime = (await getCustomMetrics(page))['mount'];
+					TestResults.addResult({component: component, type: 'Mount Time', actualValue: actualMountTime});
 				});
 			}
 		}
--- src/views/ScrollerPanel.js
+++ src/views/ScrollerPanel.js
@@ -1,15 +1,20 @@
 import kind from '@enact/core/kind';
-import Scroller from '@enact/moonstone/Scroller';
+import Scroller from '@enact/sandstone/Scroller';
+import {Profiler} from 'react';
+
+import {putRenderedMark} from '../utils';
 
 const ScrollerView = kind({
 	name: 'ScrollerView',
 
 	render: () => (
-		<div style={{height: '700px'}}>
-			<Scroller id="scroller" focusableScrollbar>
-				<div style={{height: '5000px'}} />
-			</Scroller>
-		</div>
+		<Profiler id="scroller-rendered" onRender={putRenderedMark}>
+			<div style={{height: '700px'}}>
+				<Scroller id="scroller" focusableScrollbar>
+					<div style={{height: '5000px'}} />
+				</Scroller>
+			</div>
+		</Profiler>
 	)
 });
 

6c74f773278b48ca141733a89a1dd51d5142b2f6
jeonghee27 <jeonghee27.ahn@lge.com>
2022-03-25 09:47:10 +0900

    Fix endDate to show today's results right away Enact-DCO-1.0-Signed-off-by: Jeonghee Ahn (jeonghee27.ahn@lge.com)

--- performanceMetrics/src/App/App.js
+++ performanceMetrics/src/App/App.js
@@ -180,7 +180,7 @@ const App = (props) => {
 		if (listOfTestDates.length > 0) {
 			setStartDate(convertBuildDateStringToMilis(listOfTestDates[0]));
 			// include all entries from the latest date, regardless of their hour/minute/second
-			setEndDate(convertBuildDateStringToMilis(listOfTestDates[listOfTestDates.length - 1]) + (1000 * 60 * 60 * 24));
+			setEndDate(new Date().getTime() + (1000 * 60 * 60 * 24));
 		}
 	}, [listOfTestDates]);
 
